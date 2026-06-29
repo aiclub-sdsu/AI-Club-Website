e711e3fd924b1c240dac75a231371db79520d588
EllaLamie <elamie2017@sdsu.edu>
2026-06-28 20:09:49 -0700

    Update MeetTheTeam.tsx

--- src/app/components/MeetTheTeam.tsx
+++ src/app/components/MeetTheTeam.tsx
@@ -1,15 +1,15 @@
 import { Users, ArrowLeft } from 'lucide-react';
 import { Button } from '@mui/material';
 import { Link } from 'react-router';
-import photoMandy from '../../imports/Screenshot_2026-06-14_at_7.37.29_PM.png';
-import photoPatrick from '../../imports/Screenshot_2026-06-14_at_7.37.19_PM.png';
-import photoSalimeh from '../../imports/Screenshot_2026-06-14_at_7.36.41_PM.png';
-import photoLaura from '../../imports/Screenshot_2026-06-14_at_7.36.29_PM.png';
-import photoAndrei from '../../imports/Screenshot_2026-06-14_at_7.36.16_PM.png';
-import photoAadi from '../../imports/Screenshot_2026-06-14_at_7.38.01_PM.png';
-import photoAlec from '../../imports/Screenshot_2026-06-24_at_2.13.53_PM.png';
-import photoElla from '../../imports/Screenshot_2026-06-14_at_7.37.51_PM.png';
-import photoJay from '../../imports/Screenshot_2026-06-14_at_7.37.39_PM.png';
+import photoMandy from '../../photos part 5/Screenshot_2026-06-14_at_7.37.29_PM.png';
+import photoPatrick from '../../photos part 5/Screenshot_2026-06-14_at_7.37.19_PM.png';
+import photoSalimeh from '../../photos part 5/Screenshot_2026-06-14_at_7.36.41_PM.png';
+import photoLaura from '../../photos part 5/Screenshot_2026-06-14_at_7.36.29_PM.png';
+import photoAndrei from '../../photos part 5/Screenshot_2026-06-14_at_7.36.16_PM.png';
+import photoAadi from '../../photos part 5/Screenshot_2026-06-14_at_7.38.01_PM.png';
+import photoAlec from '../../photos part 5/Screenshot_2026-06-24_at_2.13.53_PM.png';
+import photoElla from '../../photos part 5/Screenshot_2026-06-14_at_7.37.51_PM.png';
+import photoJay from '../../photos part 5/Screenshot_2026-06-14_at_7.37.39_PM.png';
 
 export default function MeetTheTeam() {
   return (
